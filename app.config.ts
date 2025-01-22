c3cd1fdf040b877b9819a9b0dc7650a33f0b77a1
Shajadul Karim Niloy <45105857+ShajadulKarim@users.noreply.github.com>
2025-01-22 15:44:30 +0600

    added 3 keyfeatures

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,9 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Project Doll',
+    title: 'MailDoll',
     description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: '/cover.png',
 
     socials: {
       twitter: 'nuxt_js',
@@ -35,13 +35,13 @@ export default defineAppConfig({
     },
 
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
       fluid: true,
-      title: 'Project Doll'
+      title: 'MailDoll'
     },
 
-    titleTemplate: 'Project Doll'
+    titleTemplate: 'MailDoll'
   }
 })
